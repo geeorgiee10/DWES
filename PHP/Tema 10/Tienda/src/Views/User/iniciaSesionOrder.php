0e1ea9ffb44b0c711d906fc0069c65d7abb1173a
Jorge Arcoya López <156781144+geeorgiee10@users.noreply.github.com>
2025-01-18 14:39:52 +0100

    Delete PHP/Tema 10 directory

--- PHP/Tema 10/Tienda/src/Views/User/iniciaSesionOrder.php
+++
@@ -1,9 +0,0 @@
-<div id="product">
-
-
-<h2>Debes iniciar sesión</h2>
-
-<p><a href="<?= BASE_URL ?>User/iniciarSesion">Inicia Sesión</a></p>
-
-
-</div>
--- PHP/Tema 10/Tienda/src/Views/error/error404.php
+++
@@ -1,8 +0,0 @@
-<hr class="hr">
-    <div class="error">
-        <h3><?= $titulo?></h3>
-
-        <p>Si quieres volver al inicio pincha aqui</p>
-        <a href="<?=  BASE_URL?>">Ir al inicio</a>
-    </div>
-<hr class="hr">
